c696ffdd7c15a029b922e757e75dc0164ff34f81
Hyun-Je Song <songhyunje@gmail.com>
2020-06-15 21:30:01 +0900

    Modify daily, longterm topic type and examples

--- topic_example.py
+++ topic_example.py
@@ -21,8 +21,8 @@ if __name__ == '__main__':
 
     searcher.count()
 
-    from_date = "2020-06-03"
-    to_date = "2020-06-04"
+    from_date = "2020-03-03"
+    to_date = "2020-03-07"
 
     # Get news id & content and then do the topic modeling
     # for news in searcher.search_by_date(from_date, to_date):
@@ -35,38 +35,40 @@ if __name__ == '__main__':
     # search_by_date for all category
     for news in searcher.search_by_date(from_date, to_date):
         news_ids.append(news.news_id)
-        topic_ids.append(random.randint(1, 5))
+    print(len(news_ids))
+
+    from_date = "2020-06-03"
+    to_date = "2020-06-04"
 
     news_ids, topic_ids = [], []
     # search_by_date given categories
     for news in searcher.search_by_date(from_date, to_date, category=['258', '259']):
         news_ids.append(news.news_id)
-        topic_ids.append(random.randint(1, 5))
+    print(len(news_ids))
 
     # label topic id per each news
-    # imbue a random topic id for test
+    # imbue random topic ids for test
     news_ids, topic_ids = [], []
-    for news in searcher.search_by_date(from_date, to_date):
+    for news in searcher.search_by_date(from_date, to_date, category=['258', '259']):
         news_ids.append(news.news_id)
-        topic_ids.append(random.randint(1, 5))
+        topic_ids.append(['D_20200101_0' + str(random.randint(1, 5)) for _ in range(random.randint(1, 3))])
 
     # update
     searcher.update_daily_topics(news_ids, topic_ids)
 
-    # get 10 news given the topic id (1)
-    for news in searcher.search_by_daily_topic(1, from_date, to_date):
-        print(news.news_id, news.title, news.daily_topic)
-        # print(news.meta.id, news.title, news.daily_topic)
-
-    # return 10 news whose topic id is 2
-    for news in searcher.search_by_daily_topic(2, from_date, to_date):
-        print(news.news_id, news.title, news.daily_topic)
-
-    sleep(3)
-    searcher.clear_daily_topic(from_date=from_date, to_date=to_date)
-    # # clear daily_topic only given news_ids
-    # searcher.clear_daily_topic(news_ids=news_ids)
-
-    sleep(3)
-    for news in searcher.search_by_daily_topic(1, from_date, to_date):
-        print(news.news_id, news.title, news.daily_topic)
+    # # get 10 news given the topic id
+    # for news in searcher.search_by_daily_topic("D_20200101_01", from_date, to_date):
+    #     print(news.news_id, news.title, news.daily_topic)
+    #
+    # # return 10 news whose topic id
+    # for news in searcher.search_by_daily_topic("D_20200101_02", from_date, to_date):
+    #     print(news.news_id, news.title, news.daily_topic)
+    #
+    # sleep(3)
+    # searcher.clear_daily_topic(from_date=from_date, to_date=to_date)
+    # # # clear daily_topic only given news_ids
+    # # searcher.clear_daily_topic(news_ids=news_ids)
+    #
+    # sleep(3)
+    # for news in searcher.search_by_daily_topic(1, from_date, to_date):
+    #     print(news.news_id, news.title, news.daily_topic)
