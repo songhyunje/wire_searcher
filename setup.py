06bce48d6cd9cb5cb8a4e851168d220c27b98bb6
Hyun-Je Song <songhyunje@gmail.com>
2020-07-23 00:40:52 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wire-searcher",  # Replace with your own username
+    version="0.0.1",
+    author="Hyun-Je Song",
+    author_email="songhyunje@gmail.com",
+    description="A search interface used in WIRE summarization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/songhyunje/wire_searcher",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
